e01afe507c778c0a257c3957dc6980553fb5505d
vifino <adinator1999@gmail.com>
2014-05-16 13:06:26 +0200

    Added kill command to let other ppl die!

---
+++ modules/owner.lua
@@ -0,0 +1,8 @@
+hooks.new("command_kill",function(cl,nick)
+	if cl.sk:getpeername()=="127.0.0.1" then
+		for k,v in pairs(clients) do
+			if v.nick == nick then
+				v:close("Client quit (Murdered by the almighty.)")
+			end
+	end
+end)
