fc83f54719de0b99378888e673132ac877eff6d7
vifino <adinator1999@gmail.com>
2014-05-16 13:07:51 +0200

    Its hook not hooks

--- modules/owner.lua
+++ modules/owner.lua
@@ -1,4 +1,4 @@
-hooks.new("command_kill",function(cl,nick)
+hook.new("command_kill",function(cl,nick)
 	if cl.sk:getpeername()=="127.0.0.1" then
 		for k,v in pairs(clients) do
 			if v.nick == nick then
