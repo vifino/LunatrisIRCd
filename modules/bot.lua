772071fa267a3fcce86e32a4812bf54fb11175bb
vifino <adinator1999@gmail.com>
2014-05-24 23:04:21 +0200

    lol

--- modules/bot.lua
+++ modules/bot.lua
@@ -4,8 +4,13 @@ hook.new("command_privmsg",function(cl,chan,txt)
 	if not chan or not txt then
 	  --Do nothing
 	else
-  	if txt:lower() == "god" then
+  	if txt:match("god") then
   	  sendtoChannel("god",chan,godreplies[ math.random( #godreplies ) ])
   	end
 	end
 end)
+-- RANDOM DERPING SPREE!
+hook.new("command_privmsg",function(cl,chan,txt) if not chan or not txt then elseif txt:match("hy") then sendtoChannel(cl.nick,chan,"Oh, sorry, its Hai...") end end)
+hook.new("command_privmsg",function(cl,chan,txt) if not chan or not txt then elseif txt:match("ty") then sendtoChannel(cl.nick,chan,"Oh, sorry, its Thank you...") end end)
+hook.new("command_privmsg",function(cl,chan,txt) if not chan or not txt then elseif txt:match("P:") then sendtoChannel(cl.nick,chan,"ITS :P GOD DAMMIT!") end end)
+hook.new("command_privmsg",function(cl,chan,txt) if not chan or not txt then elseif txt:match("P :") then sendtoChannel(cl.nick,chan,"ITS :P GOD DAMMIT!") end end)
